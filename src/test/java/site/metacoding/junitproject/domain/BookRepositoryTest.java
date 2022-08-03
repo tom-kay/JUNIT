bf4e2b606a9b97fbd3589eacb74bebf54f1cd5e2
tom-kay <74287458+tom-kay@users.noreply.github.com>
2022-08-04 00:13:11 +0900

    Repository 테스트 환경 셋팅 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,5 +1,18 @@
 package site.metacoding.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class BookRepositoryTest {
   
+  @Autowired
+  private BookRepository bookRepository;
+
+  // 1. 책등록
+  @Test
+  public void 책등록_test(){
+    System.out.println("책등록_test 실행");
+  }
 }
