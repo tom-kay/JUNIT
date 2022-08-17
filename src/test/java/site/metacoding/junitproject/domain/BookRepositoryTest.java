c3a6c24fbc6111dec25470cf5c9a2e5f6c30d68b
tom-kay <74287458+tom-kay@users.noreply.github.com>
2022-08-18 00:26:14 +0900

    레포지토리 테스트 - 책삭제하기 버그 수

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,16 +1,34 @@
 package site.metacoding.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.*;
 
 @DataJpaTest
 public class BookRepositoryTest {
   
   @Autowired
   private BookRepository bookRepository;
+
+  @BeforeEach
+  public void 데이터준비(){
+    String title = "junit";
+    String author = "겟인데어";
+
+    Book book = Book.builder()
+      .title(title)
+      .author(author)
+      .build();
+
+    bookRepository.save(book);
+  }
 
   // 1. 책등록
   @Test
@@ -28,4 +46,40 @@ public class BookRepositoryTest {
     assertEquals(title, bookPS.getTitle());
     assertEquals(author, bookPS.getAuthor());
   }
+
+  @Test
+  public void 책목록보기_test(){
+    
+    List<Book> books = bookRepository.findAll();
+    
+    System.out.println(books.size());
+  }
+
+  @Sql("classpath:db/tableInit.sql")
+  @Test
+  public void 책한건보기_test(){
+    String title = "junit";
+    String author = "겟인데어";
+
+    Book book = bookRepository.findById(1L).get();
+
+    assertEquals(title, book.getTitle());
+    assertEquals(author, book.getAuthor());
+  }
+  
+  //책 삭제
+  @Sql("classpath:db/tableInit.sql")
+  @Test
+  public void 책삭제_test(){
+    //given
+    Long id = 1L;
+
+    //when
+    bookRepository.deleteById(id);
+
+    //then
+    assertFalse(bookRepository.findById(id).isPresent());
+
+  }
+
 }
